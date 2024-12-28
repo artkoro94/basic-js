4c2d54dedbd886b5877df403d1b49780e0643d16
artkoro94 <artkoro94s@gmail.com>
2024-12-28 18:26:18 +0400

    feat: count-cats pass

--- src/count-cats.js
+++ src/count-cats.js
@@ -14,9 +14,19 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+function countCats(matrix) {
+
+  if (!Array.isArray(matrix)) {
+    return 0;
+  }
+
+  return matrix.reduce((count, row) => {
+    if (Array.isArray(row)) {
+      count += row.filter(item => item === '^^').length;
+    }
+    return count;
+  }, 0);
 }
 
 module.exports = {
